37c85ec4083011725c9f9fdd199a60b8d783223e
AmilaDev <yamilampalma@gmail.com>
2022-04-06 23:03:28 -0300

    Ts run

--- build/app.js
+++ build/app.js
@@ -5,9 +5,17 @@ const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('sec');
 const halloween = '31 Oct 2022';
 function countdown() {
-	const halloweenDate = new Date(halloween);
-	const currentDate = new Date();
-
+	const halloweenDate = +new Date(halloween);
+	const currentDate = +new Date();
+	const totalSeconds = (halloweenDate - currentDate) / 1000;
+	const days = Math.floor(totalSeconds / 3600 / 24);
+	const hours = Math.floor(totalSeconds / 3600) % 24;
+	const mins = Math.floor(totalSeconds / 60) % 60;
+	const seconds = Math.floor(totalSeconds) % 60;
+	daysEl.innerHTML = days.toString();
+	hoursEl.innerHTML = hours.toString();
+	minsEl.innerHTML = mins.toString();
+	secondsEl.innerHTML = seconds.toString();
 	console.log(halloweenDate);
 }
 countdown();
--- scripts/app.ts
+++ scripts/app.ts
@@ -9,6 +9,18 @@ function countdown() {
 	const halloweenDate = +new Date(halloween);
 	const currentDate = +new Date();
 
+	const totalSeconds: number = (halloweenDate - currentDate) / 1000;
+
+	const days: number = Math.floor(totalSeconds / 3600 / 24);
+	const hours: number = Math.floor(totalSeconds / 3600) % 24;
+	const mins: number = Math.floor(totalSeconds / 60) % 60;
+	const seconds: number = Math.floor(totalSeconds) % 60;
+
+	daysEl.innerHTML = days.toString();
+	hoursEl.innerHTML = hours.toString();
+	minsEl.innerHTML = mins.toString();
+	secondsEl.innerHTML = seconds.toString();
+
 	console.log(halloweenDate);
 }
 
